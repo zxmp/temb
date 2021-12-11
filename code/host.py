8948a03aea8e99c804081dbdcd781b79152bcb9f
zxmp <95506576+zxmp@users.noreply.github.com>
2021-12-11 20:48:48 +0100

    Create host.py

---
+++ code/host.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return "temb.glitch.me"
+
+def runhost():
+  app.run(host="0.0.0.0",port=8000)
+  
+def run(bool):
+  if bool == True:
+    t = Thread(target=runhost)
+    t.start()
+  else:
+    print("INFO: You Power Off The Host. dont use a uptimerobot now.")
